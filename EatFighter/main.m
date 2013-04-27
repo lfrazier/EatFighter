9670373573023f8571ca19b057ea9fa3e888051d
Lauren Frazier <lfrazier11@gmail.com>
2013-04-26 22:07:50 -0400

    Initial Commit

---
+++ EatFighter/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  EatFighter
+//
+//  Created by Lauren Frazier on 4/26/13.
+//  Copyright Lauren Frazier 2013. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppController");
+    [pool release];
+    return retVal;
+}
